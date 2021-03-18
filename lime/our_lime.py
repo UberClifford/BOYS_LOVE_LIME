6908cbf1d8349aa1feae285c9f8747d63bcb0615
Joakim Nøddeskov Clifford <cliffordjoakim@gmail.com>
2021-03-18 16:12:07 +0100

    added masked image gen. function + demo

--- lime/our_lime.py
+++ lime/our_lime.py
@@ -3,22 +3,23 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from PIL import Image
 from skimage.segmentation import felzenszwalb, slic, quickshift
+import numpy as np 
 
 ### IMAGE ### 
 class ImageObject():
     
     def __init__(self, original_image):
         """Initialize image object"""
-        self.original_image = original_image
-        self.fudged_image = None
+        
+        #keep image as numpy array
+        if type(original_image) == np.ndarray:
+            self.original_image = original_image
+        else:
+            self.original_image = np.array(original_image)
+        
+        self.masked_image = None
         self.superpixels = None
-                    
-    #def change_attribute(self, new_fugde_value = None, new_superpixel_value = None):
-    #    """Change attribute when fugde images and superpixels are computed in Explainer Class"""
-    #    if new_fugde_value is not None:
-    #        self.fudge_image =  new_fugde_value
-    #    if new_superpixel_value is not None:
-    #        self.superpixels = new_superpixel_value
+        self.shape = np.array(original_image).shape
 
     def show(self):
         """Display original image"""
@@ -33,8 +34,35 @@ class Explainer():
         self.segmentation_method = segmentation_method
     
     def segment_image(self, image, **kwargs):
+        """
+        image: ImageObject
+        """
         image.superpixels = self.segmentation_method(image.original_image, **kwargs)
-
+        
+        
+    def mask_image(self, image, mask_value = None):
+        """
+        Generate mask for pixels in image.
+        image: ImageObject
+        mask_value: If mask_value = None, then each masked pixel is the average
+                    of the superpixel it belongs to. Else, every pixel is set to
+                    mask_value
+        """
+        
+        img = image.original_image #get original image
+        masked_img = img #copy original image
+        superpixels = image.superpixels #get original superpixels
+        superpixel_ids = np.unique(superpixels) #get superpixels identifiers
+        
+        #set masked image pixels to average of corresponding superpixel
+        if mask_value == None:
+            for x in superpixel_ids:
+                masked_img[superpixels == x] = np.mean(img[superpixels == x], axis=0)
+        #set masked image pixels to mask_value
+        else:
+            masked_img[:] = mask_value
+        
+        image.masked_image = masked_img 
 
 ### SEGMENTATION ###
 class SegmentationMethod():
@@ -58,4 +86,3 @@ class SegmentationMethod():
         Run skimage segmentation algorithm
         """
         return self.segmentation_method(img, **kwargs)
-    
