ff0908949351757ba6932b9d8f0f4867e360b546
larsbonde <delesebastian@gmail.com>
2021-03-18 13:29:49 +0100

    added some funcs to explainer class

--- lime/our_lime.py
+++ lime/our_lime.py
@@ -5,34 +5,41 @@ from PIL import Image
 from skimage.segmentation import felzenszwalb, slic, quickshift
 
 ### IMAGE ### 
-
 class ImageObject():
     
     def __init__(self, original_image):
         """Initialize image object"""
         self.original_image = original_image
-        self.fudge_image = None
+        self.fudged_image = None
         self.superpixels = None
                     
-    def change_attribute(self, new_fugde_value = None, new_superpixel_value = None):
-        """Change attribute when fugde images and superpixels are computed in Explainer Class"""
-        if new_fugde_value is not None:
-            self.fudge_image =  new_fugde_value
-        if new_superpixel_value is not None:
-            self.superpixels = new_superpixel_value
+    #def change_attribute(self, new_fugde_value = None, new_superpixel_value = None):
+    #    """Change attribute when fugde images and superpixels are computed in Explainer Class"""
+    #    if new_fugde_value is not None:
+    #        self.fudge_image =  new_fugde_value
+    #    if new_superpixel_value is not None:
+    #        self.superpixels = new_superpixel_value
 
     def show(self):
         """Display original image"""
         img = self.original_image
         plt.imshow(img)
 
+
 ### EXPLAINER ###
+class Explainer():
+
+    def __init__(self, segmentation_method):
+        self.segmentation_method = segmentation_method
+    
+    def segment_image(self, image, **kwargs):
+        image.superpixels = self.segmentation_method(image.original_image, **kwargs)
 
 
 ### SEGMENTATION ###
 class SegmentationMethod():
 
-    def __init__(self, method="quickshift", custom_fn=None):
+    def __init__(self, method="quickshift", custom_method=None):
         """
         Set segmentation method to skimage.segmentation method
         """
@@ -43,13 +50,12 @@ class SegmentationMethod():
             self.segmentation_method = felzenszwalb
         elif self.method == "slic":
             self.segmentation_method = slic
-        if custom_fn:
-            self.segmentation_method = custom_fn
+        if custom_method:
+            self.segmentation_method = custom_method
                 
-    def segment(self, img, **kwargs):
+    def __call__(self, img, **kwargs):
         """
         Run skimage segmentation algorithm
         """
-        segments = self.segmentation_method(img, **kwargs)
-        return segments
+        return self.segmentation_method(img, **kwargs)
     
